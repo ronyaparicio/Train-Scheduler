74bc27189a947c2c06fcd0d8775e6b5fd01e83d9
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-08-02 18:48:44 -0600

    gdf

---
+++ assets/JS/app.js
@@ -0,0 +1,16 @@
+$("document").ready(function() {
+
+
+  var config = {
+    apiKey: "AIzaSyDpHPkm9wL-ytYHQFXgmrQX1MlMYP6tVOM",
+    authDomain: "train-d97c8.firebaseapp.com",
+    databaseURL: "https://train-d97c8.firebaseio.com",
+    projectId: "train-d97c8",
+    storageBucket: "",
+    messagingSenderId: "467455934899"
+  };
+  firebase.initializeApp(config);
+
+
+
+});
