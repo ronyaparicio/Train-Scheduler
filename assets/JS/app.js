3218478e6531e74fe24b73f0f03d083c72888d24
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-08-15 14:05:49 -0600

    changes

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -10,7 +10,31 @@ $("document").ready(function() {
     messagingSenderId: "467455934899"
   };
   firebase.initializeApp(config);
+  var database = firebase.database();
 
+  $("#addNewTrain").on("click", function() {
+    var name = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var trainTime = $("#trainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    database.ref().push({
+      name: name,
+      destination: destination,
+      trainTime: trainTime,
+      frequency: frequency
+    });
+
+  });
+
+
+  database.ref().on("value", function(snapshot){
+    var theChild = snapshot.val();
+    console.log(theChild.name);
+    console.log(theChild.destination);
+    console.log(theChild.trainTime);
+    console.log(theChild.frequency);
+  });
 
 
 });
